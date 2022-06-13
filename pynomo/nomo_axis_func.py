be4df2eac5606cb0c9f1162b8c8999a3f05d9fa5
Mark Mestern <mark.mestern@uswitch.com>
2022-06-13 10:45:30 +0100

    ready now

--- pynomo/nomo_axis_func.py
+++ pynomo/nomo_axis_func.py
@@ -38,7 +38,6 @@ class Axis_Wrapper:
         self.g = g
         self.start = start
         self.stop = stop
-        print("AXIS wrapper STOP",stop,"\n")
         # initial transformation coeffs
         self.set_transformation()
         self._calculate_points_()
